be92315565650a22f66b9590f7d8eb9eaceb64b1
Everaldo araujo <developer.everaldoaraujo@gmail.com>
2024-06-20 19:10:43 -0400

    update

--- service-worker.js
+++ service-worker.js
@@ -4,37 +4,34 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
         return cache.addAll(urlsToCache);
       })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
+    );
+    self.skipWaiting(); // Força o novo SW a ser ativado imediatamente
+  });
+  
+  self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+      caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
       })
-  );
-});
+    );
+    self.clients.claim(); // Faz o SW ativo controlar todas as abas imediatamente
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
