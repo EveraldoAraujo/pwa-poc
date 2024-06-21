47da74c8da1afb41b5242a42ea110a2dd6cae4e5
Everaldo araujo <developer.everaldoaraujo@gmail.com>
2024-06-20 21:22:17 -0400

    tack events

--- service-worker.js
+++ service-worker.js
@@ -6,6 +6,8 @@ const urlsToCache = [
 
 // Função para atualizar o cache
 async function updateCache(request) {
+    console.log("updateCache")
+
   const cache = await caches.open(CACHE_NAME);
   const response = await fetch(request);
   if (response.ok) {
@@ -16,6 +18,8 @@ async function updateCache(request) {
 
 // Função para buscar do cache e atualizar em segundo plano
 async function fetchAndUpdate(request) {
+    console.log("fetch e update")
+
     console.log(request);
   const cache = await caches.open(CACHE_NAME);
   const cachedResponse = await cache.match(request);
@@ -31,6 +35,8 @@ async function fetchAndUpdate(request) {
 }
 
 self.addEventListener('install', (event) => {
+    console.log("install")
+
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(urlsToCache);
@@ -40,6 +46,8 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('activate', (event) => {
+    console.log("activate")
+
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then((cacheNames) => {
@@ -56,12 +64,14 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
+    console.log("fetch")
+
   event.respondWith(fetchAndUpdate(event.request));
 });
 
 self.addEventListener('message', (event) => {
+    console.log("message")
   if (event.data.action === 'skipWaiting') {
     self.skipWaiting();
-    console.log("teste")
   }
 });
